93938c6bec05218905976e0a594fcb6b1f0e876f
Amol Dogra <amol88658@gmail.com>
2026-05-07 07:41:43 +0530

    code updates

--- bs4_scrap.py
+++ bs4_scrap.py
@@ -17,4 +17,26 @@ for ip in ip_addresses:
     print(ip)
 
 
+##updated code 
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.projecthoneypot.org/list_of_ips.php" 
+html_content = requests.get(URL)
+soup = BeautifulSoup(html_content, 'html.parser')
+
+ip_addresses = [] 
+for row in soup.select('table.manmx tr')[1:]:
+    ip_cell = row.select_one('td a.bnone') 
+    if ip_cell:
+        ip_addresses.append(ip_cell.text.strip())
+
+with open('iplist.txt', 'w') as file: 
+    for ip in ip_addresses: 
+        print(ip)
+        file.write(ip + '\n')
+
+
+
 
